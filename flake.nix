9e8c44163d258527e30ab2e60e790fa5bc371756
xgroleau 🐢 <xavgroleau@gmail.com>
2022-02-11 17:18:02 -0500

    Init repo

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Rust dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rustpkg = pkgs.rust-bin.nightly.latest.default.override {
+          targets = [ "thumbv7em-none-eabihf" ];
+        };
+
+      in with pkgs; {
+        devShell = mkShell { buildInputs = [ flip-link probe-run rustpkg ]; };
+      });
+}
