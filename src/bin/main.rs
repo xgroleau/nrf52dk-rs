893aa42e77cf92621ed3a12efd6e6dfbcb2d2238
xgroleau 🐢 <xavgroleau@gmail.com>
2022-02-19 11:37:49 -0500

    fix compilation

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,20 +2,16 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use cortex_m_rt::entry;
-use defmt;
 use defmt_rtt as _;
-use embassy::executor::Executor;
+use embassy::executor::Spawner;
 use embassy::time::{Duration, Timer};
-use embassy::util::Forever;
 use embassy_nrf::gpio::{Level, Output, OutputDrive};
 use embassy_nrf::Peripherals;
+use nrf_softdevice as _;
 use panic_probe as _;
 
-static EXECUTOR: Forever<Executor> = Forever::new();
-
-#[embassy::task]
-async fn blink(p: Peripherals) {
+#[embassy::main]
+async fn main(_spawner: Spawner, p: Peripherals) {
     let mut led = Output::new(p.P0_13, Level::Low, OutputDrive::Standard);
 
     loop {
@@ -26,17 +22,4 @@ async fn blink(p: Peripherals) {
         defmt::info!("Seting low");
         Timer::after(Duration::from_millis(300)).await;
     }
-}
-
-#[entry]
-fn main() -> ! {
-    defmt::info!("Hello World!");
-
-    let mut config = embassy_nrf::config::Config::default();
-    let p = embassy_nrf::init(config);
-
-    let executor = EXECUTOR.put(Executor::new());
-    executor.run(|spawner| {
-        defmt::unwrap!(spawner.spawn(blink(p)));
-    });
 }
